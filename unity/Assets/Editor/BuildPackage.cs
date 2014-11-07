f2b4be06aa37d440b5c307080858c506b3b7363b
Tim Leader <tim@nerfgames.co.uk>
2014-11-07 19:34:10 +0000

    Initial Commit

---
+++ unity/Assets/Editor/BuildPackage.cs
@@ -0,0 +1,20 @@
+﻿
+using System.IO;
+
+using UnityEngine;
+using UnityEditor;
+
+public class BuildPackage 
+{
+	public static string[] m_PackageInputPaths = 
+	{
+		"Assets/OBJ-IO"
+	};
+	public const string m_PackageOutputPath = "../build/obj-io.unitypackage";
+
+	[MenuItem("Build/OBJ-IO/Build Package")]
+	public static void BuildUnityPackage()
+	{
+		AssetDatabase.ExportPackage(m_PackageInputPaths, m_PackageOutputPath, ExportPackageOptions.Recurse);
+	}
+}
---
+++ unity/Assets/OBJ-IO/Plugins/Mesh/OBJ/OBJFaceVertex.cs
@@ -0,0 +1,10 @@
+﻿
+public class OBJFaceVertex
+{
+    //------------------------------------------------------------------------------------------------------------
+    public int m_VertexIndex = -1;
+    public int m_UVIndex = -1;
+    public int m_UV2Index = -1;
+    public int m_NormalIndex = -1;
+    public int m_ColorIndex = -1;
+}
